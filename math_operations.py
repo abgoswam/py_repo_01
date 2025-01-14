3f083494189749256e95003842d2a6cde420ccfa
Abhishek Goswami <abgoswam@gmail.com>
2025-01-14 09:25:09 -0800

    basic unit tests in repo

---
+++ math_operations.py
@@ -0,0 +1,13 @@
+# math_operations.py
+
+def add(a, b):
+    """
+    Returns the sum of two numbers.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    Returns the difference between two numbers.
+    """
+    return a - b
